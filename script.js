4f9c022cad74bda4bd2bb3becfc4279aa5860f37
Sagar <sagarchakrawarti24@gmail.com>
2024-03-01 00:57:55 +0530

    basic navigation

--- script.js
+++ script.js
@@ -1,12 +1,10 @@
-const timeContainer = document.getElementById("time");
 const navItems = document.querySelector(".navItems");
-function displayTime() {
-  setInterval(() => {
-    const date = new Date();
-    timeContainer.innerHTML = formattedTime(date);
-  }, 1000);
-}
-displayTime();
+const clock = document.getElementById("clock");
+const stopwatch = document.getElementById("stopwatch");
+const timer = document.getElementById("timer");
+const alarm = document.getElementById("alarm");
+
+const navs = [clock, stopwatch, timer, alarm];
 
 navItems.addEventListener("click", (event) => {
   if (event.target.tagName === "I") {
@@ -15,14 +13,24 @@ navItems.addEventListener("click", (event) => {
     console.log(event.target.id);
   }
 });
-function formattedTime(date) {
-  const hours = date.getHours();
-  const minutes = date.getMinutes();
-  const seconds = date.getSeconds();
 
-  const formattedHours = hours < 10 ? `0${hours}` : hours;
-  const formattedMinutes = minutes < 10 ? `0${minutes}` : minutes;
-  const formattedSeconds = seconds < 10 ? `0${seconds}` : seconds;
+navs.forEach((nav) =>
+  nav.addEventListener("click", () => setActivePage(nav.id))
+);
 
-  return `${formattedHours}:${formattedMinutes}:${formattedSeconds}`;
+function setActivePage(name) {
+  document.querySelector(".active-content").classList.remove("active-content");
+  document.querySelector(`#${name}-content`).classList.add("active-content");
 }
+
+// function formattedTime(date) {
+//   const hours = date.getHours();
+//   const minutes = date.getMinutes();
+//   const seconds = date.getSeconds();
+
+//   const formattedHours = hours < 10 ? `0${hours}` : hours;
+//   const formattedMinutes = minutes < 10 ? `0${minutes}` : minutes;
+//   const formattedSeconds = seconds < 10 ? `0${seconds}` : seconds;
+
+//   return `${formattedHours}:${formattedMinutes}:${formattedSeconds}`;
+// }
