6c115f0540cfbf7b8d9424785bde8f606159ced9
Sagar <sagarchakrawarti24@gmail.com>
2024-03-01 03:44:59 +0530

    added stopwatch functionality

--- script.js
+++ script.js
@@ -23,14 +23,106 @@ function setActivePage(name) {
   document.querySelector(`#${name}-content`).classList.add("active-content");
 }
 
-// function formattedTime(date) {
-//   const hours = date.getHours();
-//   const minutes = date.getMinutes();
-//   const seconds = date.getSeconds();
+function formattedTime(date) {
+  const hours = date.getHours();
+  const minutes = date.getMinutes();
+  const seconds = date.getSeconds();
 
-//   const formattedHours = hours < 10 ? `0${hours}` : hours;
-//   const formattedMinutes = minutes < 10 ? `0${minutes}` : minutes;
-//   const formattedSeconds = seconds < 10 ? `0${seconds}` : seconds;
+  const formattedHours = hours < 10 ? `0${hours}` : hours;
+  const formattedMinutes = minutes < 10 ? `0${minutes}` : minutes;
+  const formattedSeconds = seconds < 10 ? `0${seconds}` : seconds;
 
-//   return `${formattedHours}:${formattedMinutes}:${formattedSeconds}`;
-// }
+  return `${formattedHours}:${formattedMinutes}:${formattedSeconds}`;
+}
+
+function displayTime() {
+  setInterval(() => {
+    const date = new Date();
+    document.querySelector("#clock-content h1").innerHTML = formattedTime(date);
+  }, 1000);
+}
+
+function displayStopWatchTimer() {
+  let hr = 0;
+  let min = 0;
+  let sec = 0;
+  let milisec = 0;
+  let timerInterval;
+
+  const startButton = document.querySelector("#stopwatch-start");
+  const stopButton = document.querySelector("#stopwatch-stop");
+  const resetButton = document.querySelector("#stopwatch-reset");
+
+  setButtonState(false, true, true);
+
+  startButton.addEventListener("click", startStopwatch);
+  stopButton.addEventListener("click", stopStopwatch);
+  resetButton.addEventListener("click", resetStopwatch);
+
+  function startStopwatch() {
+    timerInterval = setInterval(updateStopwatch, 10);
+    setButtonState(true, false, false);
+  }
+
+  function stopStopwatch() {
+    clearInterval(timerInterval);
+    setButtonState(false, true, false);
+  }
+
+  function resetStopwatch() {
+    stopStopwatch();
+    hr = 0;
+    min = 0;
+    sec = 0;
+    milisec = 0;
+    updateStopwatchDisplay();
+    setButtonState(false, true, true);
+  }
+
+  function setButtonState(bool1, bool2, bool3) {
+    startButton.disabled = bool1;
+    stopButton.disabled = bool2;
+    resetButton.disabled = bool3;
+  }
+
+  function updateStopwatch() {
+    milisec += 10;
+    if (milisec === 1000) {
+      milisec = 0;
+      sec++;
+      if (sec === 60) {
+        sec = 0;
+        min++;
+        if (min === 60) {
+          min = 0;
+          hr++;
+        }
+      }
+    }
+    updateStopwatchDisplay();
+  }
+
+  function updateStopwatchDisplay() {
+    document.querySelector("#stopwatch-content h1").innerHTML =
+      formattedTimer();
+  }
+
+  function formattedTimer() {
+    return (
+      (hr < 10 ? "0" : "") +
+      hr +
+      ":" +
+      (min < 10 ? "0" : "") +
+      min +
+      ":" +
+      (sec < 10 ? "0" : "") +
+      sec +
+      ":" +
+      (milisec < 100 ? "00" : "") +
+      milisec
+    );
+  }
+}
+
+displayTime();
+displayStopWatchTimer();
