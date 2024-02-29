6b70ca00095b1a4d9c2382d686ca1b5f146f8a6e
Sagar <sagarchakrawarti24@gmail.com>
2024-02-29 12:34:50 +0530

    formatted time

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const navItems = document.querySelector(".navItems");
 function displayTime() {
   setInterval(() => {
     const date = new Date();
-    timeContainer.innerHTML = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+    timeContainer.innerHTML = formattedTime(date);
   }, 1000);
 }
 displayTime();
@@ -12,5 +12,17 @@ navItems.addEventListener("click", (event) => {
   if (event.target.tagName === "I") {
     document.querySelector(".active").classList.remove("active");
     event.target.classList.add("active");
+    console.log(event.target.id);
   }
 });
+function formattedTime(date) {
+  const hours = date.getHours();
+  const minutes = date.getMinutes();
+  const seconds = date.getSeconds();
+
+  const formattedHours = hours < 10 ? `0${hours}` : hours;
+  const formattedMinutes = minutes < 10 ? `0${minutes}` : minutes;
+  const formattedSeconds = seconds < 10 ? `0${seconds}` : seconds;
+
+  return `${formattedHours}:${formattedMinutes}:${formattedSeconds}`;
+}
