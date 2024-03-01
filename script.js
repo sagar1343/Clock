7638fbeb1d320e05090580a6f3784b865b733c2f
Sagar <sagarchakrawarti24@gmail.com>
2024-03-01 13:18:32 +0530

    added timer functionality

--- script.js
+++ script.js
@@ -50,27 +50,27 @@ function displayStopWatchTimer() {
   let timerInterval;
 
   const startButton = document.querySelector("#stopwatch-start");
-  const stopButton = document.querySelector("#stopwatch-stop");
+  const pauseButton = document.querySelector("#stopwatch-pause");
   const resetButton = document.querySelector("#stopwatch-reset");
 
   setButtonState(false, true, true);
 
   startButton.addEventListener("click", startStopwatch);
-  stopButton.addEventListener("click", stopStopwatch);
+  pauseButton.addEventListener("click", pauseStopwatch);
   resetButton.addEventListener("click", resetStopwatch);
 
   function startStopwatch() {
-    timerInterval = setInterval(updateStopwatch, 10);
+    timerInterval = setInterval(updateStopwatch, 1);
     setButtonState(true, false, false);
   }
 
-  function stopStopwatch() {
+  function pauseStopwatch() {
     clearInterval(timerInterval);
     setButtonState(false, true, false);
   }
 
   function resetStopwatch() {
-    stopStopwatch();
+    pauseStopwatch();
     hr = 0;
     min = 0;
     sec = 0;
@@ -81,12 +81,12 @@ function displayStopWatchTimer() {
 
   function setButtonState(bool1, bool2, bool3) {
     startButton.disabled = bool1;
-    stopButton.disabled = bool2;
+    pauseButton.disabled = bool2;
     resetButton.disabled = bool3;
   }
 
   function updateStopwatch() {
-    milisec += 10;
+    milisec += 1;
     if (milisec === 1000) {
       milisec = 0;
       sec++;
@@ -104,7 +104,89 @@ function displayStopWatchTimer() {
 
   function updateStopwatchDisplay() {
     document.querySelector("#stopwatch-content h1").innerHTML =
-      formattedTimer();
+      formattedStopwatchTimer();
+  }
+
+  function formattedStopwatchTimer() {
+    return (
+      (hr < 10 ? "0" : "") +
+      hr +
+      ":" +
+      (min < 10 ? "0" : "") +
+      min +
+      ":" +
+      (sec < 10 ? "0" : "") +
+      sec +
+      ":" +
+      (milisec < 100 ? (milisec < 10 ? "00" : "0") : "") +
+      milisec
+    );
+  }
+}
+
+function displayTimer() {
+  const timerContent = document.querySelector("#timer-content h1");
+  const timerInput = document.getElementById("timer-input");
+  let timerValue;
+  let hr = 0;
+  let min = 0;
+  let sec = 0;
+  let timerInterval;
+
+  const startButton = document.querySelector("#timer-start");
+  const pauseButton = document.querySelector("#timer-pause");
+  const resetButton = document.querySelector("#timer-reset");
+
+  startButton.addEventListener("click", startTimer);
+  pauseButton.addEventListener("click", pauseTimer);
+  resetButton.addEventListener("click", resetTimer);
+
+  timerInput.addEventListener("input", (e) => {
+    timerValue = e.target.value;
+    updateTimerValues();
+    timerContent.innerHTML = formattedTimer();
+    if (timerValue == "00:00:00") setButtonState(true, true, true);
+    else setButtonState(false, true, true);
+  });
+
+  setButtonState(true, true, true);
+
+  function updateTimerValues() {
+    hr = parseInt(timerValue.slice(0, 2));
+    min = parseInt(timerValue.slice(3, 5));
+    sec = parseInt(timerValue.slice(6, 8));
+  }
+
+  function startTimer() {
+    timerInput.disabled = true;
+    timerInterval = setInterval(() => {
+      timerContent.innerHTML = formattedTimer();
+      updateTimer();
+    }, 1000);
+    setButtonState(true, false, false);
+  }
+
+  function pauseTimer() {
+    clearInterval(timerInterval);
+    setButtonState(false, true, false);
+  }
+
+  function resetTimer() {
+    pauseTimer();
+    hr = 0;
+    min = 0;
+    sec = 0;
+    updateTimer();
+    timerContent.innerHTML = "00:00:00";
+    setButtonState(false, true, true);
+    timerInput.value = "00:00:00";
+    timerInput.disabled = false;
+  }
+
+  function setButtonState(bool1, bool2, bool3) {
+    startButton.disabled = bool1;
+    pauseButton.disabled = bool2;
+    resetButton.disabled = bool3;
   }
 
   function formattedTimer() {
@@ -116,13 +198,31 @@ function displayStopWatchTimer() {
       min +
       ":" +
       (sec < 10 ? "0" : "") +
-      sec +
-      ":" +
-      (milisec < 100 ? "00" : "") +
-      milisec
+      sec
     );
+  }
+
+  function updateTimer() {
+    if (sec === 0) {
+      if (min === 0) {
+        if (hr === 0) {
+          clearInterval(timerInterval);
+          timerContent.innerHTML = "Times-Up";
+        } else {
+          hr--;
+          min = 59;
+          sec = 59;
+        }
+      } else {
+        min--;
+        sec = 59;
+      }
+    } else {
+      sec--;
+    }
   }
 }
 
 displayTime();
 displayStopWatchTimer();
+displayTimer();
